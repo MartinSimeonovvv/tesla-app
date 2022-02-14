de2278d4e193b01bd858b76eb873732c243fbc6c
MartinSimeonovvv <marti_191997@abv.bg>
2022-02-14 12:23:08 +0200

    Adding more sections

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,54 @@ import Section from "./Section";
 function Home() {
   return (
     <Container>
-      <Section />
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroungImg="model-s.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        backgroungImg="model-y.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        backgroungImg="model-3.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        backgroungImg="model-x.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Lowest Cost Solar Panels in America"
+        description="Money-back guarantee"
+        backgroungImg="solar-panel.jpg"
+        leftBtnText="Order now"
+        rightBtnText="Learn more"
+      />
+      <Section
+        title="Solar for New Roofs"
+        description="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
+        backgroungImg="solar-roof.jpg"
+        leftBtnText="Order now"
+        rightBtnText="Learn more"
+      />
+      <Section
+        title="Accessories"
+        description=""
+        backgroungImg="accessories.jpg"
+        leftBtnText="Shop now"
+      />
     </Container>
   );
 }
